1e75c336c730e57534dd1916d559b91c0a87cba0
otabek05 <otabektoshmukhammedov@gmail.com>
2023-02-14 21:23:33 +0900

    updated

--- BLOG/settings.py
+++ BLOG/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-*-x8itxj(h9%qfo8)1k31aig)0s29_0+0oiobu2b09#8@6^ob7'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = ["https://blog-production-cf65.up.railway.app/"]
+ALLOWED_HOSTS = ["https://blog-production-cf65.up.railway.app"]
 
 
 # Application definition
