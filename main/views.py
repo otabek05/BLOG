b8d54d6f8ccca936d3bdfbdedc212b91317ac551
otabek05 <otabektoshmukhammedov@gmail.com>
2023-02-15 21:24:35 +0900

    all

--- main/views.py
+++ main/views.py
@@ -1,8 +1,8 @@
 from django.shortcuts import render, redirect
 from django.contrib.auth.decorators import login_required
 
-from .models import Todo
 
+from .models import Todo
 @login_required(login_url='login')
 def index(request):
     
